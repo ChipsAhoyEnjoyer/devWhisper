fc68628ded7a8899abfe04833edcb0973bbf49b5
ChipsAhoyEnjoyer <erick.j.john@gmail.com>
2025-08-16 15:53:50 -0400

    new: auth for logging in/endpoint auth validation

---
+++ internal/server/auth.go
@@ -0,0 +1,122 @@
+package server
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var hashCost = getHashCost()
+
+const (
+	issuer = "devWhisper"
+)
+
+func getHashCost() int {
+	godotenv.Load(".env")
+	costStr := os.Getenv("HASH_COST")
+	if costStr == "" {
+		log.Println("Warning: HASH_COST environment variable is not a valid integer, using default cost of 10")
+		costStr = "10" // default cost
+	}
+	cost, err := strconv.Atoi(costStr)
+	if err != nil {
+		log.Printf("Warning: error using strconv.Atoi to convert hash cost to integer: %v\n", err)
+		cost = 10
+	}
+	return cost
+}
+
+func HashPassword(password string) (hashedPW string, err error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(password), hashCost)
+	return string(b), err
+}
+
+func CheckPasswordHash(hash, password string) (err error) {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	tk := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.RegisteredClaims{
+			Issuer:    issuer,
+			Subject:   userID.String(),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		},
+	)
+	tkString, err := tk.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return tkString, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	tk, err := jwt.ParseWithClaims(
+		tokenString,
+		jwt.MapClaims{},
+		func(tk *jwt.Token) (any, error) {
+			if tk.Method != jwt.SigningMethodHS256 {
+				return nil, errors.New("error incorrect signing method given: " + tk.Method.Alg())
+			}
+			return []byte(tokenSecret), nil
+		},
+	)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	issuer, err := tk.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != string(issuer) {
+		return uuid.Nil, errors.New("invalid issuer")
+	}
+	idStr, err := tk.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		return uuid.Nil, errors.New("invalid user id")
+	}
+	return id, nil
+}
+
+func GetBearerToken(headers http.Header) (string, error) {
+	bearer := headers.Get("Authorization")
+	if !strings.HasPrefix(bearer, "Bearer ") {
+		return "", errors.New("no bearer token found")
+	}
+	return strings.TrimPrefix(bearer, "Bearer "), nil
+}
+
+func GetAPIKey(headers http.Header) (string, error) {
+	key := headers.Get("Authorization")
+	if !strings.HasPrefix(key, "ApiKey ") {
+		return "", errors.New("no bearer token found")
+	}
+	return strings.TrimPrefix(key, "ApiKey "), nil
+}
+
+func MakeRefreshToken() (string, error) {
+	tk := make([]byte, 32)
+	_, err := rand.Read(tk)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(tk), nil
+}
