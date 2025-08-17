a039964db780626b2be1b989517e8f7699d3fa1c
ChipsAhoyEnjoyer <erick.j.john@gmail.com>
2025-08-17 12:41:46 -0400

    changes: Updated homepage handler

--- internal/server/handleHomepage.go
+++ internal/server/handleHomepage.go
@@ -7,6 +7,12 @@ import (
 
 func HandleHomepage(w http.ResponseWriter, r *http.Request) {
 	log.Println("User has landed in the homepage")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Welcome to devWhisper"))
+	payload := struct {
+		Message string `json:"response"`
+	}{Message: "Welcome to devWhisper!"}
+	RespondWithJSON(
+		w,
+		http.StatusOK,
+		payload,
+	)
 }
